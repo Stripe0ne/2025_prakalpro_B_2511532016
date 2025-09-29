00b553b9d606afe53b7f889dca98bb2b2f2886b4
Muhammad Yasin Habiburrahman <abikdanebo@gmail.com>
2025-09-29 11:12:43 +0700

    Praktikum Pekan 4

---
+++ src/pekan4/multiIf.java
@@ -0,0 +1,29 @@
+package pekan4;
+import java.util.Scanner;
+
+public class multiIf {
+	public static void main(String[] args) {
+	int umur;
+	char sim;
+	Scanner a = new Scanner(System.in);
+	System.out.println("Input Umur Anda: ");
+	umur = a.nextInt();
+	System.out.println("Apakah Anda Sudah Punya SIM C: ");
+	sim=a.next().charAt(0);
+	a.close();
+	
+	if(umur >= 17 && sim=='y') {
+		System.out.println("Anda Sudah Dewasa dan Boleh Bawa Motor");
+	}
+	if(umur >= 17 && sim!='y'){
+		System.out.println("Anda Sudah Dewasa tetapi Tidak Boleh Bawa Motor");
+	}
+	if(umur < 17 &&  sim=='y'){
+		System.out.println("Anda Belum Cukup Umur Bawa Motor");
+	}
+	if(umur < 17 && sim!='y'){
+		System.out.println("Anda Belum Cukup Umur punya SIM");
+	}
+	}
+
+}
