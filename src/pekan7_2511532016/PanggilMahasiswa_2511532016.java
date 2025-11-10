0ee2f77a55d984d59e7ba4e49a47f603df971f21
Muhammad Yasin Habiburrahman <abikdanebo@gmail.com>
2025-11-10 11:37:07 +0700

    Add files via upload

---
+++ src/pekan7_2511532016/PanggilMahasiswa_2511532016.java
@@ -0,0 +1,12 @@
+package pekan7_2511532016;
+
+public class PanggilMahasiswa_2511532016 {
+	public static void main(String[] args) {
+		Mahasiswa_2511532016 a = new Mahasiswa_2511532016();
+		a.setNim(2016);
+		a.setNama("Abique");
+		System.out.println(a.getNim());
+		System.out.println(a.getNama());
+		a.Cetak();
+	}
+}
---
+++ src/pekan7_2511532016/String1_2511532016.java
@@ -0,0 +1,14 @@
+package pekan7_2511532016;
+
+public class String1_2511532016 {
+
+	public static void main(String[] args) {
+		String salam = "Assalamualaikum";
+		System.out.println("Panjang salam adalah " + salam.length());
+		System.out.println(salam.toUpperCase());
+		System.out.println(salam.toLowerCase());
+		System.out.println(salam.indexOf("salam"));
+		
+	}
+
+}
